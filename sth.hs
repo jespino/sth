4199fd20648859b9a4d6bdee39f6b9068abe6c73
Jesús Espino <jesus.espino@kaleidos.net>
2013-12-17 18:13:41 +0100

    Reorganizing a little

--- sth.hs
+++ sth.hs
@@ -50,20 +50,8 @@ flags =
         "Print the help message"
     ]
 
-parse argv = case getOpt Permute flags argv of
-    (args, fs, []) -> do
-        let files = if null fs then [] else fs
-        if Help `elem` args
-            then do hPutStrLn stderr (usageInfo header flags)
-                    exitSuccess
-            else return (L.nub args, files)
-    (_, _, errs) -> do
-        hPutStrLn stderr (concat errs ++ usageInfo header flags)
-        exitWith (ExitFailure 1)
-    where header = "Usage: sth [options] [files]"
-
 showStats :: [Float] -> [Flags] -> [String]
-showStats list [] = []
+showStats _ [] = []
 showStats list (Count:xs) = ("Count: " ++ show (STHLib.count list)) : showStats list xs
 showStats list (Mean:xs) = ("Mean: " ++ show (STHLib.mean list)) : showStats list xs
 showStats list (Stddev:xs) = ("Stddev: " ++ show (STHLib.stddev list)) : showStats list xs
@@ -77,7 +65,22 @@ showStats list (Q3:xs) = ("Q3: " ++ show (STHLib.q3 list)) : showStats list xs
 showStats list (Max:xs) = ("Max: " ++ show (STHLib.max list)) : showStats list xs
 
 contentToFloats :: String -> [Float]
-contentToFloats str = map (\x -> read x :: Float) $ lines str
+contentToFloats = (map read) . lines
+
+-- Mutable zone
+
+parse argv = case getOpt Permute flags argv of
+    (args, fs, []) -> do
+        let files = if null fs then [] else fs
+        if Help `elem` args
+            then do hPutStrLn stderr (usageInfo header flags)
+                    exitSuccess
+            else return (L.nub args, files)
+    (_, _, errs) -> do
+        hPutStrLn stderr (concat errs ++ usageInfo header flags)
+        exitWith (ExitFailure 1)
+    where header = "Usage: sth [options] [files]"
+
 
 readFiles :: [FilePath] -> IO String
 readFiles = fmap concat . mapM readFile
@@ -90,4 +93,4 @@ main = do
     (as, fs) <- getArgs >>= parse
     content <- getData fs
 
-    putStr $ unlines $ showStats (contentToFloats content) (if null as then allStats else as )
+    putStr $ unlines $ showStats (contentToFloats content) (if null as then allStats else as)
