969240508cd937c819eb34cbfbadd1c9a3676dbb
Jesús Espino <jesus.espino@kaleidos.net>
2013-12-17 19:18:15 +0100

    Now working transpose and complete parameters

--- sth.hs
+++ sth.hs
@@ -2,6 +2,7 @@ import System.Console.GetOpt
 import System.Environment
 import System.Exit
 import System.IO
+import Text.Printf
 import qualified Data.List as L
 import qualified STHLib
 
@@ -20,6 +21,8 @@ data Flags
     | Max
     | Help
     | Summary
+    | Complete
+    | Transpose
     deriving (Eq,Ord,Enum,Show,Bounded)
 
 allStats = [Count, Mean, Stddev, Stderr, Sum, Var, Min, Q1, Median, Q3, Max]
@@ -50,6 +53,10 @@ flags =
         "Display the maximun"
     ,Option [] ["summary"]    (NoArg Summary)
         "Display the summary info"
+    ,Option [] ["complete"]    (NoArg Complete)
+        "Display the complete info"
+    ,Option [] ["transpose-output", "tn"]    (NoArg Transpose)
+        "Display the info in vertical mode"
     ,Option "h" ["help"]    (NoArg Help)
         "Print the help message"
     ]
@@ -67,22 +74,57 @@ showStats list (Q1:xs) = ("Q1: " ++ show (STHLib.q1 list)) : showStats list xs
 showStats list (Median:xs) = ("Median: " ++ show (STHLib.median list)) : showStats list xs
 showStats list (Q3:xs) = ("Q3: " ++ show (STHLib.q3 list)) : showStats list xs
 showStats list (Max:xs) = ("Max: " ++ show (STHLib.max list)) : showStats list xs
-showStats list (Summary:xs) = showStats list xs
+showStats list (Transpose:xs) = showStats list xs
+
+showStatsHeaders :: [Flags] -> String
+showStatsHeaders [] = ""
+showStatsHeaders (Count:xs) = printf "%10s" "Count" ++ showStatsHeaders xs
+showStatsHeaders (Mean:xs) = printf "%10s" "Mean" ++ showStatsHeaders xs
+showStatsHeaders (Stddev:xs) = printf "%10s" "Stddev" ++ showStatsHeaders xs
+showStatsHeaders (Stderr:xs) = printf "%10s" "Stderr" ++ showStatsHeaders xs
+showStatsHeaders (Sum:xs) = printf "%10s" "Sum" ++ showStatsHeaders xs
+showStatsHeaders (Var:xs) = printf "%10s" "Variance" ++ showStatsHeaders xs
+showStatsHeaders (Min:xs) = printf "%10s" "Min" ++ showStatsHeaders xs
+showStatsHeaders (Q1:xs) = printf "%10s" "Q1" ++ showStatsHeaders xs
+showStatsHeaders (Median:xs) = printf "%10s" "Median" ++ showStatsHeaders xs
+showStatsHeaders (Q3:xs) = printf "%10s" "Q3" ++ showStatsHeaders xs
+showStatsHeaders (Max:xs) = printf "%10s" "Max" ++ showStatsHeaders xs
+showStatsHeaders (Transpose:xs) = showStatsHeaders xs
+
+showHStats :: [Float] -> [Flags] -> String
+showHStats _ [] = ""
+showHStats list (Count:xs) = printf "%10f" (STHLib.count list) ++ showHStats list xs
+showHStats list (Mean:xs) = printf "%10f" (STHLib.mean list) ++ showHStats list xs
+showHStats list (Stddev:xs) = printf "%10f" (STHLib.stddev list) ++ showHStats list xs
+showHStats list (Stderr:xs) = printf "%10f" (STHLib.stderr list) ++ showHStats list xs
+showHStats list (Sum:xs) = printf "%10f" (STHLib.sum list) ++ showHStats list xs
+showHStats list (Var:xs) = printf "%10f" (STHLib.variance list) ++ showHStats list xs
+showHStats list (Min:xs) = printf "%10f" (STHLib.min list) ++ showHStats list xs
+showHStats list (Q1:xs) = printf "%10f" (STHLib.q1 list) ++ showHStats list xs
+showHStats list (Median:xs) = printf "%10f" (STHLib.median list) ++ showHStats list xs
+showHStats list (Q3:xs) = printf "%10f" (STHLib.q3 list) ++ showHStats list xs
+showHStats list (Max:xs) = printf "%10f" (STHLib.max list) ++ showHStats list xs
+showHStats list (Transpose:xs) = showHStats list xs
 
 contentToFloats :: String -> [Float]
 contentToFloats = map read . lines
 
 -- Mutable zone
 
+populateArgs :: [Flags] -> [Flags]
+populateArgs [] = []
+populateArgs (Summary:xs) = summaryStats ++ populateArgs xs
+populateArgs (Complete:xs) = allStats ++ populateArgs xs
+populateArgs (x:xs) = x : populateArgs xs
+
 parse argv = case getOpt Permute flags argv of
-    (as, fs, []) -> do
+    (args, fs, []) -> do
         let files = if null fs then [] else fs
-        let args = if Summary `elem` as then as ++ summaryStats else as
 
         if Help `elem` args
             then do hPutStrLn stderr usage
                     exitSuccess
-            else return (L.nub args, files)
+            else return (L.nub (populateArgs args), files)
     (_, _, errs) -> do
         hPutStrLn stderr (concat errs ++ usage)
         exitWith (ExitFailure 1)
@@ -101,4 +143,7 @@ main = do
     (as, fs) <- getArgs >>= parse
     content <- getData fs
 
-    putStr $ unlines $ showStats (contentToFloats content) (if null as then allStats else as)
+    if Transpose `elem` as
+        then putStr $ unlines $ showStats (contentToFloats content) (if null [ x | x <- as, x /= Transpose] then allStats else as)
+        else do putStrLn $ showStatsHeaders (if null as then allStats else as)
+                putStrLn $ showHStats (contentToFloats content) (if null as then allStats else as)
